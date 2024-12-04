e84a43bcc77386834a98d2c8aef5d8fdd514f039
vkovtun <viktor.kovtun@id.ethz.ch>
2024-12-04 21:53:24 +0100

    Added English top 3000 words.

--- 3/sentence_generator_gpt.py
+++ 3/sentence_generator_gpt.py
@@ -2,18 +2,9 @@ from transformers import GPT2LMHeadModel, GPT2Tokenizer
 import torch
 
 # Load the pre-trained GPT-2 model and tokenizer
-model_name = 'gpt2-medium'  # You can also use 'gpt2-medium', 'gpt2-large', etc.
+model_name = 'gpt2-xl'  # You can also use 'gpt2-medium', 'gpt2-large', etc.
 tokenizer = GPT2Tokenizer.from_pretrained(model_name)
 model = GPT2LMHeadModel.from_pretrained(model_name)
-
-pre_primer = ["a", "and", "away", "big", "blue", "can", "come", "down", "find", "for", "funny", "go", "help", "here", "I", "in", "is", "it", "jump", "little", "look", "make", "me", "my", "not", "one", "play", "red", "run", "said", "see", "the", "three", "to", "two", "up", "we", "where", "yellow", "you"]
-primer = ["all", "am", "are", "at", "ate", "be", "black", "brown", "but", "came", "did", "do", "eat", "four", "get", "good", "have", "he", "into", "like", "must", "new", "no", "now", "on", "our", "out", "please", "pretty", "ran", "ride", "saw", "say", "she", "so", "soon", "that", "there", "they", "this", "too", "under", "want", "was", "well", "went", "what", "white", "who", "will", "with", "yes"]
-grade_1 = ["after", "again", "an", "any", "as", "ask", "by", "could", "every", "fly", "from", "give", "going", "had", "has", "her", "him", "his", "how", "just", "know", "let", "live", "may", "of", "old", "once", "open", "over", "put", "round", "some", "stop", "take", "thank", "them", "then", "think", "walk", "were", "when"]
-grade_2 = ["always", "around", "because", "been", "before", "best", "both", "buy", "call", "cold", "does", "don't", "fast", "first", "five", "found", "gave", "goes", "green", "its", "made", "many", "off", "or", "pull", "read", "right", "sing", "sit", "sleep", "tell", "their", "these", "those", "upon", "us", "use", "very", "wash", "which", "why", "wish", "work", "would", "write", "your"]
-grade_3 = ["about", "better", "bring", "carry", "clean", "cut", "done", "draw", "drink", "eight", "fall", "far", "full", "got", "grow", "hold", "hot", "hurt", "if", "keep", "kind", "laugh", "light", "long", "much", "myself", "never", "only", "own", "pick", "seven", "shall", "show", "six", "small", "start", "ten", "today", "together", "try", "warm"]
-nouns = ["apple", "baby", "back", "ball", "bear", "bed", "bell", "bird", "birthday", "boat", "box", "boy", "bread", "brother", "cake", "car", "cat", "chair", "chicken", "children", "Christmas", "coat", "corn", "cow", "day", "dog", "doll", "door", "duck", "egg", "eye", "farm", "farmer", "father", "feet", "fire", "fish", "floor", "flower", "game", "garden", "girl", "goat", "grass", "ground", "hand", "head", "hill", "home", "horse", "house", "kitty", "leg", "letter", "man", "men", "milk", "money", "morning", "mother", "name", "nest", "night", "paper", "party", "picture", "pig", "rabbit", "rain", "ring", "robin", "Santa Claus", "school", "seed", "sheep", "shoe", "sister", "snow", "song", "squirrel", "stick", "street", "sun", "table", "thing", "time", "top", "toy", "tree", "watch", "water", "way", "wind", "window", "woman", "women", "wood"]
-punctuation=[".", ",", "?", "!", ";", ":", "-"]
-
 
 # Define your limited vocabulary
 # limited_vocab = [
@@ -24,19 +15,37 @@ punctuation=[".", ",", "?", "!", ";", ":", "-"]
 #     'looked', 'found', 'thought', 'knew', 'felt', 'time', 'world', 'story'
 # ]
 
-limited_vocab = pre_primer + primer + grade_1 + grade_2 + grade_3 + nouns + punctuation
+pre_primer = ["a", "and", "away", "big", "blue", "can", "come", "down", "find", "for", "funny", "go", "help", "here", "I", "in", "is", "it", "jump", "little", "look", "make", "me", "my", "not", "one", "play", "red", "run", "said", "see", "the", "three", "to", "two", "up", "we", "where", "yellow", "you"]
+primer = ["all", "am", "are", "at", "ate", "be", "black", "brown", "but", "came", "did", "do", "eat", "four", "get", "good", "have", "he", "into", "like", "must", "new", "no", "now", "on", "our", "out", "please", "pretty", "ran", "ride", "saw", "say", "she", "so", "soon", "that", "there", "they", "this", "too", "under", "want", "was", "well", "went", "what", "white", "who", "will", "with", "yes"]
+grade_1 = ["after", "again", "an", "any", "as", "ask", "by", "could", "every", "fly", "from", "give", "going", "had", "has", "her", "him", "his", "how", "just", "know", "let", "live", "may", "of", "old", "once", "open", "over", "put", "round", "some", "stop", "take", "thank", "them", "then", "think", "walk", "were", "when"]
+grade_2 = ["always", "around", "because", "been", "before", "best", "both", "buy", "call", "cold", "does", "don't", "fast", "first", "five", "found", "gave", "goes", "green", "its", "made", "many", "off", "or", "pull", "read", "right", "sing", "sit", "sleep", "tell", "their", "these", "those", "upon", "us", "use", "very", "wash", "which", "why", "wish", "work", "would", "write", "your"]
+grade_3 = ["about", "better", "bring", "carry", "clean", "cut", "done", "draw", "drink", "eight", "fall", "far", "full", "got", "grow", "hold", "hot", "hurt", "if", "keep", "kind", "laugh", "light", "long", "much", "myself", "never", "only", "own", "pick", "seven", "shall", "show", "six", "small", "start", "ten", "today", "together", "try", "warm"]
+nouns = ["apple", "baby", "back", "ball", "bear", "bed", "bell", "bird", "birthday", "boat", "box", "boy", "bread", "brother", "cake", "car", "cat", "chair", "chicken", "children", "Christmas", "coat", "corn", "cow", "day", "dog", "doll", "door", "duck", "egg", "eye", "farm", "farmer", "father", "feet", "fire", "fish", "floor", "flower", "game", "garden", "girl", "goat", "grass", "ground", "hand", "head", "hill", "home", "horse", "house", "kitty", "leg", "letter", "man", "men", "milk", "money", "morning", "mother", "name", "nest", "night", "paper", "party", "picture", "pig", "rabbit", "rain", "ring", "robin", "Santa Claus", "school", "seed", "sheep", "shoe", "sister", "snow", "song", "squirrel", "stick", "street", "sun", "table", "thing", "time", "top", "toy", "tree", "watch", "water", "way", "wind", "window", "woman", "women", "wood"]
 
-# Get the IDs of the words in your vocabulary
+limited_vocab = pre_primer + primer + grade_1 + grade_2 + grade_3 + nouns
+
+# with open('en_top_3000.txt', 'r') as file:
+#     limited_vocab = [line.strip() for line in file]
+
+punctuation=[".", ",", "?", "!", ";", ":", "-", "(", ")", "'", '"']
+limited_vocab += punctuation
+
+
+# Get the IDs of the words and punctuation in your vocabulary
 limited_vocab_ids = []
 for word in limited_vocab:
-    tokens = tokenizer.encode(' ' + word, add_special_tokens=False)
+    # Handle both with and without prefix space
+    tokens = tokenizer.encode(word, add_special_tokens=False)
+    tokens_with_space = tokenizer.encode(' ' + word, add_special_tokens=False)
     limited_vocab_ids.extend(tokens)
+    limited_vocab_ids.extend(tokens_with_space)
 
 limited_vocab_ids = list(set(limited_vocab_ids))  # Remove duplicates
 
 # Create a set of all token IDs and determine the forbidden tokens
 all_token_ids = set(tokenizer.get_vocab().values())
-forbidden_token_ids = all_token_ids - set(limited_vocab_ids)
+allowed_token_ids = set(limited_vocab_ids)
+forbidden_token_ids = all_token_ids - allowed_token_ids
 
 # Convert forbidden token IDs into the format required by 'bad_words_ids'
 bad_words_ids = [[token_id] for token_id in forbidden_token_ids]
@@ -48,7 +57,7 @@ def generate_constrained_text(prompt, max_length=100):
         input_ids,
         max_length=max_length,
         do_sample=True,
-        temperature=0.8,
+        temperature=0.4,
         top_k=50,
         top_p=0.95,
         bad_words_ids=bad_words_ids,
@@ -60,6 +69,6 @@ def generate_constrained_text(prompt, max_length=100):
     return generated_text
 
 # Generate a story
-prompt = "Once upon a time"
-story = generate_constrained_text(prompt, max_length=200)
+prompt = "Long time ago"
+story = generate_constrained_text(prompt, max_length=35)
 print(story)
--- 3/silly_sentence_generator.py
+++ 3/silly_sentence_generator.py
@@ -2,23 +2,43 @@ import random
 
 # Define your vocabulary
 vocabulary = {
-    'Noun': ['dog', 'cat', 'man', 'woman', 'apple', 'park', 'car'],
-    'Verb': ['eats', 'drives', 'sees', 'likes', 'loves', 'hates', 'finds'],
-    'Adjective': ['big', 'small', 'red', 'green', 'fast', 'slow'],
-    'Determiner': ['the', 'a'],
-    'Preposition': ['in', 'on', 'under', 'with', 'without']
+    'Noun': ["apple", "baby", "back", "ball", "bear", "bed", "bell", "bird", "birthday", "boat", "box", "boy", "bread", "brother", "cake", "car",
+             "cat", "chair", "chicken", "children", "Christmas", "coat", "corn", "cow", "day", "dog", "doll", "door", "duck", "egg", "eye", "farm",
+             "farmer", "father", "feet", "fire", "fish", "floor", "flower", "game", "garden", "girl", "goat", "grass", "ground", "hand", "head",
+             "hill", "home", "horse", "house", "kitty", "leg", "letter", "man", "men", "milk", "money", "morning", "mother", "name", "nest", "night",
+             "paper", "party", "picture", "pig", "rabbit", "rain", "ring", "robin", "Santa Claus", "school", "seed", "sheep", "shoe", "sister",
+             "snow", "song", "squirrel", "stick", "street", "sun", "table", "thing", "time", "top", "toy", "tree", "watch", "water", "way", "wind",
+             "window", "woman", "women", "wood"],
+    'Verb': ["am", "are", "ate", "be", "came", "can", "come", "did", "do", "does", "don't", "draw", "drink", "eat", "fall", "fly", "found", "gave",
+             "get", "give", "go", "goes", "grow", "had", "has", "have", "help", "hurt", "is", "jump", "keep", "know", "laugh", "let", "like", "live",
+             "look", "made", "make", "may", "must", "pick", "play", "put", "ran", "read", "ride", "run", "saw", "say", "see", "show", "sing", "sit",
+             "sleep", "start", "take", "tell", "thank", "think", "try", "use", "walk", "want", "was", "went", "were", "will", "wish", "work",
+             "write"],
+    'Adjective': ["big", "black", "blue", "brown", "clean", "cold", "fast", "first", "five", "four", "funny", "good", "green", "hot", "kind", "light",
+                  "little", "long", "many", "much", "new", "old", "one", "pretty", "red", "round", "seven", "six", "small", "ten", "three", "two",
+                  "white", "yellow"],
+    'Determiner': ["a", "an", "any", "her", "his", "its", "my", "one", "our", "some", "that", "the", "their", "these", "this", "those", "your"],
+    'Preposition': ["about", "around", "at", "before", "by", "for", "from", "in", "into", "of", "off", "on", "out", "over", "to", "under", "upon",
+                    "with"],
+    'Pronoun': ["I", "he", "him", "me", "she", "they", "us", "we", "who", "you"],
+    'Conjunction': ["and", "but", "or", "so", "because"],
+    'Adverb': ["again", "always", "away", "here", "just", "never", "no", "not", "now", "soon", "there", "together", "too", "very", "well", "where",
+               "why", "yes", "yet"]
 }
 
 grammar = {
-    'S': [['NP', 'VP']],
-    'NP': [['Determiner', 'Noun'], ['Determiner', 'Adjective', 'Noun']],
-    'VP': [['Verb', 'NP'], ['Verb', 'PP']],
-    'PP': [['Preposition', 'NP']],
+    'S': [['NP', 'VP'], ['Pronoun', 'VP'], ['NP', 'VP', 'Conjunction', 'S'], ['VP']],
+    'NP': [['Determiner', 'Noun'], ['Determiner', 'Adjective', 'Noun'], ['Pronoun'], ['Adjective', 'Noun'], ['NP', 'PP'], ['NP', 'Conjunction', 'NP']],
+    'VP': [['Verb', 'NP'], ['Verb', 'PP'], ['Verb', 'Adverb'], ['Verb'], ['Verb', 'Adverb', 'PP'], ['Verb', 'NP', 'Conjunction', 'VP']],
+    'PP': [['Preposition', 'NP'], ['Preposition', 'Determiner', 'Noun']],
     'Determiner': vocabulary['Determiner'],
     'Noun': vocabulary['Noun'],
     'Verb': vocabulary['Verb'],
     'Adjective': vocabulary['Adjective'],
-    'Preposition': vocabulary['Preposition']
+    'Preposition': vocabulary['Preposition'],
+    'Pronoun': vocabulary['Pronoun'],
+    'Conjunction': vocabulary['Conjunction'],
+    'Adverb': vocabulary['Adverb'],
 }
 
 def generate(symbol):
