eb4cdfbb82c54d885d512df9c3afbc9ba2dd99a3
vkovtun <viktor.kovtun@id.ethz.ch>
2024-12-12 12:27:11 +0100

    Added extra words to improve the correctness of the score.

--- 3/sentence_generator_gpt.py
+++ 3/sentence_generator_gpt.py
@@ -1,5 +1,3 @@
-from pprint import pprint
-
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 import nltk
 from nltk.tokenize import word_tokenize
@@ -96,14 +94,15 @@ primer = ["all", "am", "are", "at", "ate", "be", "black", "brown", "but", "came"
 grade_1 = ["after", "again", "an", "any", "as", "ask", "by", "could", "every", "fly", "from", "give", "going", "had", "has", "her", "him", "his", "how", "just", "know", "let", "live", "may", "of", "old", "once", "open", "over", "put", "round", "some", "stop", "take", "thank", "them", "then", "think", "walk", "were", "when"]
 grade_2 = ["always", "around", "because", "been", "before", "best", "both", "buy", "call", "cold", "does", "don't", "fast", "first", "five", "found", "gave", "goes", "green", "its", "made", "many", "off", "or", "pull", "read", "right", "sing", "sit", "sleep", "tell", "their", "these", "those", "upon", "us", "use", "very", "wash", "which", "why", "wish", "work", "would", "write", "your"]
 grade_3 = ["about", "better", "bring", "carry", "clean", "cut", "done", "draw", "drink", "eight", "fall", "far", "full", "got", "grow", "hold", "hot", "hurt", "if", "keep", "kind", "laugh", "light", "long", "much", "myself", "never", "only", "own", "pick", "seven", "shall", "show", "six", "small", "start", "ten", "today", "together", "try", "warm"]
-nouns = ["apple", "baby", "back", "ball", "bear", "bed", "bell", "bird", "birthday", "boat", "box", "boy", "bread", "brother", "cake", "car", "cat", "chair", "chicken", "children", "Christmas", "coat", "corn", "cow", "day", "dog", "doll", "door", "duck", "egg", "eye", "farm", "farmer", "father", "feet", "fire", "fish", "floor", "flower", "game", "garden", "girl", "goat", "grass", "ground", "hand", "head", "hill", "home", "horse", "house", "kitty", "leg", "letter", "man", "men", "milk", "money", "morning", "mother", "name", "nest", "night", "paper", "party", "picture", "pig", "rabbit", "rain", "ring", "robin", "Santa Claus", "school", "seed", "sheep", "shoe", "sister", "snow", "song", "squirrel", "stick", "street", "sun", "table", "thing", "time", "top", "toy", "tree", "watch", "water", "way", "wind", "window", "woman", "women", "wood"]
+nouns = ["apple", "baby", "back", "ball", "bear", "bed", "bell", "bird", "birthday", "boat", "box", "boy", "bread", "brother", "cake", "car", "cat", "chair", "chicken", "children", "christmas", "coat", "corn", "cow", "day", "dog", "doll", "door", "duck", "egg", "eye", "farm", "farmer", "father", "feet", "fire", "fish", "floor", "flower", "game", "garden", "girl", "goat", "grass", "ground", "hand", "head", "hill", "home", "horse", "house", "kitty", "leg", "letter", "man", "men", "milk", "money", "morning", "mother", "name", "nest", "night", "paper", "party", "picture", "pig", "rabbit", "rain", "ring", "robin", "santa", "school", "seed", "sheep", "shoe", "sister", "snow", "song", "squirrel", "stick", "street", "sun", "table", "thing", "time", "top", "toy", "tree", "watch", "water", "way", "wind", "window", "woman", "women", "wood"]
+extra = ["ago", "child"]
 
-limited_vocab = pre_primer + primer + grade_1 + grade_2 + grade_3 + nouns
+limited_vocab = pre_primer + primer + grade_1 + grade_2 + grade_3 + nouns + extra
 
 # with open('en_top_3000.txt', 'r') as file:
 #     limited_vocab = [line.strip() for line in file]
 
-punctuation = ["ago", ".", ",", "?", "!", ";", ":", "-", "(", ")", "'", '"']
+punctuation = [".", ",", "?", "!", ";", ":", "-", "(", ")", "'", '"']
 limited_vocab += punctuation
 
 
@@ -131,13 +130,14 @@ prompt = "Long time ago"
 
 # Evaluation
 
-# Download required datasets
+# Downloading required datasets
 nltk.download('punkt')
+nltk.download('punkt_tab')
 nltk.download('wordnet')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('averaged_perceptron_tagger_eng')
 
 for i in range(5):
     story = generate_constrained_text(prompt, max_length=50)
-    print(f"{i}: {story}")
+    print(f"{i + 1}: {story}")
     print(f"Vocabulary accuracy: {compute_accuracy(story, limited_vocab)}")
