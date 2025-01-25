2f53ec7bf9412eb5db98fb2c23642d2562a79bb2
Viktor Kovtun <victor.kovtun@gmail.com>
2025-01-25 17:43:57 +0100

    Committed required files for the project.

--- 4/test_spacy_file.py
+++ 4/test_spacy_file.py
@@ -2,7 +2,6 @@ import spacy
 from spacy.tokens import DocBin
 
 # Load your spaCy binary data
-# doc_bin = DocBin().from_disk("/home/viktor/Work/Projects/UniBe/CAS-NLP/4/en/validation/validation1.spacy")
 doc_bin = DocBin().from_disk("/home/viktor/Work/Projects/UniBe/CAS-NLP/4/en/train/train1.spacy")
 
 # Load a blank NLP model (adjust the language as needed, e.g., "en")
