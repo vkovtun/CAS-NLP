974f302c027c5fc60a442ba633a7ac53b39bf3c1
Viktor Kovtun <victor.kovtun@gmail.com>
2025-03-22 16:56:20 +0100

    Added support of downloading an external file from Google Drive.

--- 6/mner_home.py
+++ 6/mner_home.py
@@ -1,10 +1,34 @@
 import streamlit as st
 import spacy
+import os
+import zipfile
+import gdown
 from pathlib import Path
 
-# model = spacy.load(Path('model_wikianc_uk_2/model-best'))
-# print("Pipeline components:", model.pipe_names)
-# print(model)
+FILE_ID="1mleDunOQr-pvRfPq7Q5pzjlz3Kc0eX2y"
+DEST_ZIP = "model_wikianc_uk_2.zip"
+DEST_DIR = "model_wikianc_uk_2"
+
+def download_and_extract():
+    if os.path.isdir(DEST_DIR):
+        print(f"'{DEST_DIR}' already exists. Skipping download.")
+        return
+
+    # Construct the URL
+    url = f"https://drive.google.com/uc?id={FILE_ID}"
+    print("Downloading ZIP file...")
+    gdown.download(url, DEST_ZIP, quiet=False)
+
+    print("Unzipping...")
+    with zipfile.ZipFile(DEST_ZIP, 'r') as zip_ref:
+        zip_ref.extractall(DEST_DIR)
+
+    print("Cleaning up...")
+    os.remove(DEST_ZIP)
+
+    print(f"Done! Files are in '{DEST_DIR}'.")
+
+download_and_extract()
 
 if 'model' not in st.session_state:
     model = spacy.load(Path('model_wikianc_uk_2/model-best'))
