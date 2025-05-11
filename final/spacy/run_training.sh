153440777086426fa59f6aa90eff3de5b7ee7e32
Viktor Kovtun <victor.kovtun@gmail.com>
2025-05-11 21:13:53 +0200

    Fixing the error in the bash script.

--- final/spacy/run_training.sh
+++ final/spacy/run_training.sh
@@ -54,7 +54,7 @@ for index in $(seq 1 $max_index); do
 
     if [ -d "${out_dir}/model-last" ]; then
         cfg_file="config/wikianc/${language}_resume.cfg"
-        prev_out_dir = "models/wikianc/${language}_prev"
+        prev_out_dir="models/wikianc/${language}_prev"
         rm -rf -- ${prev_out_dir}
         mv -- ${out_dir} ${prev_out_dir}
     else
