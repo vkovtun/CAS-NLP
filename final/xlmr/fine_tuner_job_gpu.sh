7edb536ae75cbbb41daa8cc5c0e2c5029aa988a8
Viktor Kovtun <victor.kovtun@gmail.com>
2025-05-18 16:23:16 +0200

    First version of finetuner of xlmr with the wikianc dataset.

---
+++ final/xlmr/fine_tuner_job_gpu.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#SBATCH --job-name="TNER Fine Tuning"
+#SBATCH --time=24:00:00
+#SBATCH --mem-per-cpu=32GB
+#SBATCH --cpus-per-task=2
+
+#_SBATCH --qos=job_gpu_preemptable
+#_SBATCH --partition=gpu-invest
+
+#_SBATCH --qos=job_gpu
+#SBATCH --partition=gpu
+
+#_SBATCH --gres=gpu:rtx3090:1
+#_SBATCH --gres=gpu:rtx4090:1
+#SBATCH --gres=gpu:a100:1
+#_SBATCH --gres=gpu:h100:1
+#SBATCH --array=0-10
+
+# Slavic languages we consider:
+# bg – Bulgarian (South Slavic)
+# bs – Bosnian (South Slavic)
+# cs – Czech (West Slavic)
+# hr – Croatian (South Slavic)
+# mk – Macedonian (South Slavic)
+# pl – Polish (West Slavic)
+# ru – Russian (East Slavic)
+# sk – Slovak (West Slavic)
+# sl – Slovenian (South Slavic)
+# sr – Serbian (South Slavic)
+# uk – Ukrainian (East Slavic)
+
+languages=("bg" "bs" "cs" "hr" "mk" "pl" "ru" "sk" "sl" "sr" "uk")
+
+echo "Launched at $(date)"
+echo "Job ID: ${SLURM_JOBID}"
+echo "Node list: ${SLURM_NODELIST}"
+echo "Submit dir.: ${SLURM_SUBMIT_DIR}"
+echo "Numb. of cores: ${SLURM_CPUS_PER_TASK}"
+
+echo "Fine turing for language ${languages[$SLURM_ARRAY_TASK_ID]}"
+
+# Running the actual job
+python3 -m xlmr_fine_tuner_wikianc.py --language ${languages[$SLURM_ARRAY_TASK_ID]}
