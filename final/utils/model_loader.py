063ad0aaf5959b768dd6246005f3c7d670adc3f7
Viktor Kovtun <victor.kovtun@gmail.com>
2025-05-31 13:42:52 +0200

    Made the language download work.

--- final/utils/model_loader.py
+++ final/utils/model_loader.py
@@ -1,68 +1,88 @@
-import spacy
-from huggingface_hub import snapshot_download
+import os
+import re
+import zipfile
 from pathlib import Path
+
+import gdown
+import spacy
 import streamlit as st
 
-# TODO: add language selector
-# DEST_ZIP = "model_wikianc_uk_2.zip"
-WIKIANN_PATH = "spacy/models/wikiann"
+MODELS_PATH = "spacy1/models"
+WIKIANN_DIR = "wikiann"
 MODEL_BEST_DIR = "model-best"
-# MODEL_FILE = f"{MODEL_BEST_DIR}/transformer/model"
 
-# TODO: Uncomment these later and make sure it works with a file from Google Drive.
+def download(file_id, output):
+    try:
+        print("Attempting download...", flush=True)
+        url = f"https://drive.google.com/uc?id={file_id}"
+        gdown.download(url, output, quiet=False)
+        print("Download finished. Checking contents...", flush=True)
+    except Exception as e:
+        raise RuntimeError(f"Download failed with exception: {e}")
 
-# def download(file_id, output):
-#     try:
-#         print("Attempting download...", flush=True)
-#         url = f"https://drive.google.com/uc?id={file_id}"
-#         gdown.download(url, output, quiet=False)
-#         print("Download finished. Checking contents...", flush=True)
-#         print(os.listdir(), flush=True)
-#     except Exception as e:
-#         print("Download failed with exception:", flush=True)
-#         import traceback
-#         traceback.print_exc()
-#
-#
-# def unzip(file, output):
-#     with zipfile.ZipFile(file, 'r') as zip_ref:
-#         zip_ref.extractall(output)
-#
-# def download_and_extract():
-#     zip_file_id = os.environ['ZIP_FILE_ID']
-#     model_file_id = os.environ['MODEL_FILE_ID']
-#
-#     print("Downloading ZIP file...")
-#     download(zip_file_id, DEST_ZIP)
-#
-#     print("Unzipping...")
-#     unzip(DEST_ZIP, DEST_DIR)
-#
-#     print("Contents of DEST_DIR:")
-#     print(os.listdir(DEST_DIR))
-#
-#     print("Contents of model-best:")
-#     print(os.listdir(f"{DEST_DIR}/model-best"))
-#
-#     print("Contents of transformer:")
-#     print(os.listdir(f"{DEST_DIR}/model-best/transformer"))
-#
-#     print("Downloading model file...")
-#     download(model_file_id, MODEL_FILE)
-#
-#     print("Contents of model directory:")
-#     print(os.listdir(DEST_DIR))
-#
-#     print("Cleaning up...")
-#     os.remove(DEST_ZIP)
-#
-#     print(f"Done! Files are in '{DEST_DIR}'.")
+
+def get_file_id(folder_id, language):
+    target_pattern = re.compile(f"^{WIKIANN_DIR}/{language}.zip$")
+
+    # 1 List the folder contents without downloading files
+    files = gdown.download_folder(
+        id=folder_id,
+        skip_download=True,          # only metadata
+        quiet=True,
+        remaining_ok=True            # ignore the 50-file hard cap warning
+    )
+
+    # 2 Pick the file you care about
+    matches = [f.id for f in files if target_pattern.match(f.path)]
+
+    if not matches:
+        raise ValueError("Pattern not found in folder!")
+    if len(matches) > 1:
+        raise ValueError("More than one file match the pattern.")
+
+    return matches[0]
+
+
+def unzip(file, output):
+    with zipfile.ZipFile(file, "r") as zip_ref:
+        zip_ref.extractall(output)
+
+
+def download_and_extract(language):
+    models_folder_id = os.environ["MODELS_FOLDER_ID"]
+
+    print(f"Downloading ZIP file for language {language}...")
+    wikiann_path = Path(MODELS_PATH, WIKIANN_DIR)
+    model_file_id = get_file_id(models_folder_id, language)
+
+    wikiann_path.mkdir(parents=True, exist_ok=True)
+    zip_file_path = Path(wikiann_path, f"{language}.zip")
+    download(model_file_id, str(zip_file_path))
+
+    dest_dir = Path(MODELS_PATH, WIKIANN_DIR, language)
+    print(f"Unzipping {zip_file_path}...")
+    unzip(zip_file_path, dest_dir)
+
+    print("Contents of DEST_DIR:")
+    print(os.listdir(dest_dir))
+
+    print("Cleaning up...")
+    os.remove(zip_file_path)
+
+    print(f"Done! Files are in '{dest_dir}'.")
+
 
 @st.cache_resource
 def load_model(language):
     try:
         print("Attempting to load SpaCy model...", flush=True)
-        model = spacy.load(Path(WIKIANN_PATH, language, MODEL_BEST_DIR))
+        path = Path(MODELS_PATH, WIKIANN_DIR, language, MODEL_BEST_DIR)
+
+        if not path.exists():
+            print(f"Path {str(path)} does not exist. Fetching the model from Google Drive.", flush=True)
+            download_and_extract(language)
+
+        model = spacy.load(path)
         print("SpaCy model loaded!", flush=True)
         return model
     except Exception as e:
